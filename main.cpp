c78f64e0c227e54a5cac011865f268ac0d299dc9
Wojciech Lepich <wolepp@gmail.com>
2019-02-05 23:42:11 +0100

    troche zmian

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
+void wypiszInfo()
+{
+    std::cout << "no elo" << std::endl;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
+    std::cout << "No co tam? " << std::endl;
     std::cout << "Test gita" << std::endl;
+
+    std::cout << "alksje f" << std::endl;
+
+    std::cout << "ddd" << std::endl;
     return 0;
 }
